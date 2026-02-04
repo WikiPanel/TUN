f7b52ff5e5078e385ec0b6f521ea112cf7ba3863
WikiPanel <39990803+WikiPanel@users.noreply.github.com>
2026-02-05 01:57:04 +0330

    Create GRE-Kharej.sh

---
+++ GRE-Kharej.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+read -p "Enter IRAN public IP: " IR_IP
+read -p "Enter KHAREJ public IP: " KH_IP
+
+cat <<EOF > /etc/netplan/gre1.yaml
+network:
+  version: 2
+  renderer: networkd
+
+  tunnels:
+    gre1:
+      mode: gre
+      local: ${KH_IP}
+      remote: ${IR_IP}
+      addresses:
+        - 10.10.10.1/30
+      mtu: 1476
+EOF
+
+netplan generate
+netplan apply
