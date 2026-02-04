94d943d0dfa287b0e7f540d3a5829503616196a7
WikiPanel <39990803+WikiPanel@users.noreply.github.com>
2026-02-05 01:57:51 +0330

    Create GRE+Gost-Iran.sh

---
+++ GRE+Gost-Iran.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+read -p "Enter IRAN public IP: " IR_IP
+read -p "Enter KHAREJ public IP: " KH_IP
+
+cat <<EOF > /etc/netplan/gre1.yaml
+network:
+  version: 2
+  renderer: networkd
+
+  tunnels:
+    gre1:
+      mode: gre
+      local: ${IR_IP}
+      remote: ${KH_IP}
+      addresses:
+        - 10.10.10.2/30
+      mtu: 1476
+EOF
+
+netplan generate
+netplan apply
+
+bash <(curl -Ls https://raw.githubusercontent.com/WikiPanel/gost/main/gost.sh) <<'EOF'
+1
+10.10.10.1
+1
+443,8443,2087,444,587,465,8951
+1
+2
+EOF
